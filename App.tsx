e2f15c5873dc199c42dcc1a577a1bd3f40a5fafd
c4mpos-dev <campos.eet@gmail.com>
2024-10-09 15:28:26 -0300

    Theme.ts added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import { StatusBar } from 'react-native';
 import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import theme from 'src/theme/'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Groups/>
-    </>
+    </ThemeProvider>
   );
 }
