fa8017397efa2bcf01a8b8a99f177ed5b040cfcd
c4mpos-dev <campos.eet@gmail.com>
2024-10-09 16:39:59 -0300

    Loading Component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import { StatusBar, ActivityIndicator } from 'react-native';
+import { StatusBar } from 'react-native';
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import theme from 'src/theme/'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -14,7 +15,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups/> : <ActivityIndicator/>}
+      {fontsLoaded ? <Groups/> : <Loading/>}
     </ThemeProvider>
   );
 }
