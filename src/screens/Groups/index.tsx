c1611edcafb6aa4315f834773ec2b66360bdfde1
c4mpos-dev <campos.eet@gmail.com>
2024-10-16 17:47:47 -0300

    FlatList added

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da RocketSeat']);
+
   return (
     <Container>
       <Header/>
@@ -13,7 +17,11 @@ export function Groups() {
         subtitle='Jogue com a sua turma'
       />
 
-      <GroupCard title='Galera do Ignite'/>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item}/>}
+      />
     </Container>
   );
 }
