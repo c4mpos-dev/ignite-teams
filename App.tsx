f66f41419c8559b8c06c8bd8d26a7d2cbc3e3c08
c4mpos-dev <campos.eet@gmail.com>
2024-10-17 17:20:54 -0300

    Screen NewGroup added

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import theme from 'src/theme/'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups/> : <Loading/>}
+      {fontsLoaded ? <NewGroup/> : <Loading/>}
     </ThemeProvider>
   );
 }
