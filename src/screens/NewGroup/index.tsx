2bcaf00281766b54e9fbe0b40ecdaa896b8cea0a
c4mpos-dev <campos.eet@gmail.com>
2024-10-18 16:44:29 -0300

    Component Input added and css helper

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup(){
     return(
@@ -13,10 +14,8 @@ export function NewGroup(){
                     title="Nova turma"
                     subtitle="Crie a turma para adicionar as pessoas"
                 />
-                <Button
-                    title="Criar"
-                />
-
+                <Input placeholder="Nome da turma"/>
+                <Button title="Criar"/>
             </Content>
         </Container>
     );
