f222f7061cb4b8fd309ae9e50c4adc2d2ba6e73b
c4mpos-dev <campos.eet@gmail.com>
2024-10-18 17:55:03 -0300

    Component ButtonIcon added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'react-native';
-import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import theme from 'src/theme/'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
+import { Groups } from '@screens/Groups';
 import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup/> : <Loading/>}
+      {fontsLoaded ? <Players/> : <Loading/>}
     </ThemeProvider>
   );
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,6 @@
 import { TextInputProps } from "react-native";
 import { useTheme } from "styled-components/native";
-import { Container } from "./style";
+import { Container } from "./styles";
 
 export function Input({...rest}: TextInputProps){
     const { COLORS } = useTheme();
