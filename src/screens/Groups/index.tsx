8305916e35dabd9e744269ef7f6d3f06b11b3748
c4mpos-dev <campos.eet@gmail.com>
2024-10-22 18:58:00 -0300

    Open group/class

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -9,7 +9,6 @@ import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
-
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
 
@@ -29,6 +28,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string){
+    navigation.navigate("players", { group });
+  }
+
   useFocusEffect(useCallback(() => {
     fetchGroups();
   }, []));
@@ -45,7 +48,12 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={item => item}
-        renderItem={({ item }) => <GroupCard title={item}/>}
+        renderItem={({ item }) => 
+          <GroupCard 
+            title={item}
+            onPress={() => handleOpenGroup(item)}
+          />
+        }
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?"/>}
       />
