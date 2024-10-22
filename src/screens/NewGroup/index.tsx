9a86cf2bde97462c141c751047d8a105bd44c0dc
c4mpos-dev <campos.eet@gmail.com>
2024-10-22 17:34:38 -0300

    UseCallback and useFocusEffect added

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 import { Container, Content, Icon } from "./styles";
+import { groupCreate } from "@storage/group/groupCreate";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
-import { useNavigation } from "@react-navigation/native";
+import { err } from "react-native-svg";
+
+
 
 export function NewGroup(){
     const [group, setGroup] = useState('');
 
     const navigation = useNavigation();
 
-    function handleNew(){
-        navigation.navigate("players", { group });
+    async function handleNew(){
+        try {
+            await groupCreate(group);
+            navigation.navigate("players", { group });
+        }
+        catch(error) {
+            console.log(error);
+        }
     };
-
 
     return(
         <Container>
